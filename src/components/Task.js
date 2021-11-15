270f5dc614c9813f8ed58857ddee5739786de543
Leandro Dantas <leandrosuy@gmail.com>
2021-11-15 09:29:16 -0300

    fixbug tarefa atrasada

--- src/components/Task.js
+++ src/components/Task.js
@@ -7,16 +7,21 @@ const Task = ({ task, onDelete }) => {
     const novaData = moment(task.data).format('DD-MMM-YY');
     const dataAtual = moment(new Date()).format('YYYY-MM-DD');
     const [alerta, setAlerta] = useState(false)
+    const [atrasada, setAtrasada] = useState(false);
 
     useEffect(() => {
         if (task.data === dataAtual) {
             setAlerta(true)
+        }
+        if(task.data < dataAtual){
+            setAtrasada(true)
         }
     },[dataAtual, task.data])
 
     return (
         <>
             <div className='task'>
+            {atrasada ? <p className='textoAtrasada'>Tarefa atrasada</p> : null}
             {alerta ? <p className='textoAlert'>Tarefa de hoje</p> : null}
                 <h3>{task.tarefa} <FaTimes style={{ color: 'red', cursor: 'pointer' }} onClick={() => { onDelete(task._id) }} /></h3>
                 <p>{novaData}</p>
